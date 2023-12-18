426af0a0c0b67b59164fd043afea74b9693a9a6b
carlosmoreno4470777 <carlosmoreno.4470777@gmail.com>
2023-12-17 20:03:23 -0600

    menu commit changes

--- week3/src/com/promineotech/borrador.java
+++ week3/src/com/promineotech/borrador.java
@@ -1,40 +1,471 @@
 package com.promineotech;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class borrador {
+
+	private static String[] newArr;
 
 	public static void main(String[] args) {
 
-		//System.out.println("Hello World");
+		// System.out.println("Hello World");
+
+		/*
+		 * String firstName = "Mary"; String lastName = "Brown"; int assignmentOne =
+		 * 100; int assignmentTwo = 79; int assignmentThree = 80; int assignmentFour =
+		 * 90; int assignmentFive = 100;
+		 * 
+		 * System.out.println("Student: " + firstName + " " + lastName);
+		 * System.out.println("Grades:"); System.out.println("1: " + assignmentOne);
+		 * System.out.println("2: " + assignmentTwo); System.out.println("3: " +
+		 * assignmentThree); System.out.println("4: " + assignmentFour);
+		 * System.out.println("5: " + assignmentFive);
+		 */
+		/*
+		 * String fullName = "Mary" + " " + "Brown"; int[] gradesArray = {100, 79, 80,
+		 * 90, 100}; int counter = 1;
+		 * 
+		 * System.out.println("Student: " + fullName); System.out.println("Grades: ");
+		 * for (int grade : gradesArray) { System.out.println((counter++) + ": " +
+		 * grade); }
+		 * 
+		 */
+		/*
+		 * Scanner in = new Scanner(System.in); int num1, num2, num3; num1 =
+		 * in.nextInt(); num2 = in.nextInt(); num3 = in.nextInt();
+		 * System.out.println(returnSum(num1, num2, num3));
+		 */
+
+		/*
+		 * Scanner in = new Scanner(System.in); int numTimes; String userWord; userWord
+		 * = in.nextLine(); numTimes = in.nextInt();
+		 * System.out.println(repeatWord(userWord,numTimes));
+		 */
+
+		// System.out.println(divideNums(5, 0));
+
+		/*
+		 * Scanner in = new Scanner(System.in); int num1; num1 = in.nextInt();
+		 * fizzBuzz(num1);
+		 */
+
+		/*
+		 * int a = 10, b = 10, c = 10; System.out.println(returnSum(a,b,c));
+		 */
+
+		/*
+		 * Scanner in = new Scanner(System.in); double product1, product2, product3,
+		 * taxRate; product1 = in.nextDouble(); product2 = in.nextDouble(); product3 =
+		 * in.nextDouble(); taxRate = in.nextDouble();
+		 * System.out.println("Your order total is: $" + String.format("%,.2f",
+		 * orderTotal(product1, product2, product3, taxRate)));
+		 */
+
+		/*
+		 * Scanner in = new Scanner(System.in); int a,b,c,d,e; a = in.nextInt(); b =
+		 * in.nextInt(); c = in.nextInt(); d = in.nextInt(); e = in.nextInt(); int[]
+		 * mainArray = createArray(a, b, c, d, e);
+		 * System.out.println("The third number in your array is: " + mainArray[2]);
+		 */
+
+		/*
+		 * Scanner in = new Scanner(System.in); int digits = in.nextInt(); int sumAll =
+		 * sumOfDigits(digits);
+		 * System.out.println("The sum of all digits for the value " + digits + " is " +
+		 * sumAll + ".");
+		 */
+
+		/*
+		 * Scanner in = new Scanner(System.in); String password = in.nextLine(); boolean
+		 * meetsRequirement = passwordChecker(password); if (meetsRequirement) {
+		 * System.out.println(password + " meets the requirements."); } else {
+		 * System.out.println(password + " does not meet the requirements."); }
+		 */
+
+		// String iCanRetire = readyForTheBeach(64);
+		// System.out.println(iCanRetire);
+
+		/*
+		 * Scanner in = new Scanner(System.in); String[] names; names = new String[5];
+		 * names[0] = in.nextLine(); names[1] = in.nextLine(); names[2] = in.nextLine();
+		 * names[3] = in.nextLine(); names[4] = in.nextLine();
+		 * 
+		 * int avgLength = avgNameLength(names);
+		 * System.out.println("The average name length is " + avgLength + ".");
+		 * 
+		 */
+
+		/*
+		 * Scanner in = new Scanner(System.in); int length = in.nextInt(); int width =
+		 * in.nextInt(); int perimeter = rectanglePerimeter(length, width);
+		 * System.out.println("The rectangle has a perimeter of " + perimeter + ".");
+		 */
+
+		// System.out.println(whatsReturned(9, "Hi, There", false, 4.99, 'T'));
+
+		/*
+		 * int[] numbers = {2, 4, 6, 8, 10}; int sum = 0;
+		 * 
+		 * for (int i = 0; i < numbers.length; i++) { sum = sum + numbers[i]; }
+		 * 
+		 * System.out.println(sum);
+		 */
+
+		/*
+		 * Scanner in = new Scanner(System.in); int x; int[] arr = { 5, 10, 44, -4, 10
+		 * }; x = in.nextInt(); addX(arr, x);
+		 */
 		
 		/*
-		String firstName = "Mary";
-		String lastName = "Brown";
-		int assignmentOne = 100;
-		int assignmentTwo = 79;
-		int assignmentThree = 80;
-		int assignmentFour = 90;
-		int assignmentFive = 100;
+		 * Scanner in = new Scanner(System.in); int a,b; a = in.nextInt(); b =
+		 * in.nextInt(); int[] arr = { 3, 7, 0, 4, -6, 10, 55 }; changeTwo(a, b, arr);
+		 */
+		
+		/*
+		 * Scanner in = new Scanner(System.in); String word; word = in.nextLine();
+		 * charArray(word);
+		 */
+		
+		/*
+		 * Scanner in = new Scanner(System.in); String userWord;; userWord =
+		 * in.nextLine(); System.out.println("Your array is: ");
+		 * System.out.println(Arrays.toString(wordToArray(userWord)));
+		 */
+		
+		/*
+		 * Scanner in = new Scanner(System.in); int num; num = in.nextInt();
+		 * System.out.println("Your array is: ");
+		 * System.out.println(Arrays.toString(buildArray(num)));
+		 */
+		
+		/*
+		 * Scanner in = new Scanner(System.in); int num1, num2, num3, num4, num5, num6;
+		 * num1 = in.nextInt(); num2 = in.nextInt(); num3 = in.nextInt(); num4 =
+		 * in.nextInt(); num5 = in.nextInt(); num6 = in.nextInt(); int returnedInt =
+		 * createInt(num1, num2, num3, num4, num5, num6);
+		 * System.out.println("The last number of the array is " + returnedInt + ".");
+		 */
+		
+		/*
+		 * Scanner in = new Scanner(System.in); String[] arr = {"Astros", "Reds",
+		 * "Phillies", "Dodgers", "Angels"}; String word1, word2, word3; word1 =
+		 * in.nextLine(); word2 = in.nextLine(); word3 = in.nextLine(); newArray(word1,
+		 * word2, word3, arr);
+		 */
+		
+		/*
+		 * Scanner in = new Scanner(System.in); int a,b,c,d,e; a = in.nextInt(); b =
+		 * in.nextInt(); c = in.nextInt(); d = in.nextInt(); e = in.nextInt();
+		 * reverseArray(a, b, c, d, e);
+		 */
+		
+		Scanner in = new Scanner(System.in);
+		int input1, input2;
+		input1 = in.nextInt();
+		input2 = in.nextInt();
+		multidimensional(input1, input2);
+		
+		
+	}// END OF MAIN ++++++++++++++++++++++++++++
 
-		System.out.println("Student: " + firstName + " " + lastName);
-		System.out.println("Grades:");
-		System.out.println("1: " + assignmentOne);
-		System.out.println("2: " + assignmentTwo);
-		System.out.println("3: " + assignmentThree);
-		System.out.println("4: " + assignmentFour);
-		System.out.println("5: " + assignmentFive);
-		*/
-		
-		String fullName = "Mary" + " " + "Brown"; 
-		int[] gradesArray = {100, 79, 80, 90, 100};
-		int counter = 1;
+	private static void multidimensional(int a, int b) {
 
-		System.out.println("Student: " + fullName);
-		System.out.println("Grades: ");
-		for (int grade : gradesArray) {
-		System.out.println((counter++) + ": " + grade);
-		}
+        // Check for error - both numbers need to be greater than 2
+        if (a <= 2 || b <= 2) {
+            System.out.println("Both numbers need to be greater than 2.");
+            return; // Don't proceed if invalid parameters
+        }
+
+        // Declare the multidimensional array based on user inputs
+        int[][] arr = new int[a][b];
+
+        // Loop through each row (outer loop)
+        for (int i = 0; i < a; i++) {
+
+            // Loop through each column within the row (inner loop)
+            for (int j = 0; j < b; j++) {
+
+                // Store the current index value in the array
+                arr[i][j] = j; // Alternatively, you can assign any desired value here
+            }
+        }
+
+        // Print the last element of the first row using appropriate indexing
+        System.out.println(arr[0][arr[0].length - 1]);
 		
-		
-	}//END OF MAIN
+	}
+
+	
+	
+	/*
+	 * private static void reverseArray(int a, int b, int c, int d, int e) {
+	 * 
+	 * int[] arr = {a, b, c, d, e};
+	 * 
+	 * // Create a new array to store the reversed elements int[] reverseArr = new
+	 * int[arr.length];
+	 * 
+	 * 
+	 * // Loop through the original array in reverse order for (int i = arr.length -
+	 * 1, j = 0; i >= 0; i--, j++) { reverseArr[j] = arr[i]; // Copy element from
+	 * original array to reversed array }
+	 * 
+	 * for (int i = 0; i < arr.length; i++) { System.out.println(arr[i]); }
+	 * 
+	 * for (int i = 0; i < reverseArr.length; i++) {
+	 * System.out.println(reverseArr[i]); }
+	 * 
+	 * }
+	 */
+
+    /*
+	static int[] reverseArray(int[] arr) {
+        // Create a new array to store the reversed elements
+        int[] reverseArr = new int[arr.length];
+
+        // Loop through the original array in reverse order
+        for (int i = arr.length - 1, j = 0; i >= 0; i--, j++) {
+            reverseArr[j] = arr[i]; // Copy element from original array to reversed array
+        }
+
+        // Return the reversed array
+        return reverseArr;
+    }
+    */
+	
+	/*
+	 * static void newArray(String word1, String word2, String word3, String[] arr)
+	 * {
+	 * 
+	 * // Create a new array to hold the three new words String[] newArr = {word1,
+	 * word2, word3}; // Calculate the total size of the combined array int
+	 * combinedSize = arr.length + newArr.length; // Create a new array to hold all
+	 * the elements String[] combinedArray = new String[combinedSize];
+	 * 
+	 * int loopCounter = 0;
+	 * 
+	 * // ADDING THE arr ARRAY FROM MAIN INTO THE LOCAL COMBINE ARRAY for (int i =
+	 * 0; i < arr.length; i++) { combinedArray[i] = arr[loopCounter]; loopCounter++;
+	 * }
+	 * 
+	 * loopCounter = 0; for (int i = combinedArray.length - newArr.length; i <
+	 * combinedArray.length; i++) {
+	 * 
+	 * combinedArray[i] = newArr[loopCounter]; loopCounter++; }
+	 * 
+	 * // Print all elements of the combined array for (String word : combinedArray)
+	 * { System.out.println(word); }
+	 * 
+	 * }
+	 */
+	
+	/*
+	 * static int createInt(int num1, int num2, int num3, int num4, int num5, int
+	 * num6) {
+	 * 
+	 * int[] arrayInt = {num1, num2, num3, num4, num5,num6};
+	 * 
+	 * return arrayInt[5]; }
+	 */
+	
+	/*
+	 * static int[] buildArray(int num) { // take the int value stored in the given
+	 * variable num and //declare an Array of that size called numArray. int[]
+	 * numArray = new int[num];
+	 * 
+	 * // Write a for loop that starts with a loop variable or index of zero (0)
+	 * //and checks to see if the loop variable is less than num for (int i = 0; i <
+	 * num; i++) { numArray[i] = i; // add the current loop variable } return
+	 * numArray; }
+	 */
+
+	
+	
+	/*
+	 * static char[] wordToArray(String userWord) { char[] charArray = new
+	 * char[userWord.length()];
+	 * 
+	 * // Loop through each character in the userWord and add it to the array for
+	 * (int i = 0; i < userWord.length(); i++) { charArray[i] = userWord.charAt(i);
+	 * } return charArray; }
+	 */
+	
+	
+	/*
+	 * private static void charArray(String userWord) { // Initialize a new
+	 * character array with the length of the userWord char[] charsWord = new
+	 * char[userWord.length()];
+	 * 
+	 * // Add each letter of userWord to the Array as elements for (int i = 0; i <
+	 * userWord.length(); i++) { charsWord[i] = userWord.charAt(i); }
+	 * 
+	 * for (char c : charsWord) { System.out.println(c); }
+	 * 
+	 * }
+	 */
+
+	
+	/*
+	 * private static void changeTwo(int a, int b, int[] arr) { int firstPart=0; int
+	 * lastPart=0;
+	 * 
+	 * //first letter replace arr[0]=a; //last letter replace arr[arr.length - 1]=b;
+	 * 
+	 * for (int i = 0; i < arr.length; i++) { System.out.println(arr[i]); }
+	 * 
+	 * }
+	 */
+
+	
+	/*
+	 * private static void addX(int[] arr, int x) {
+	 * System.out.println("**Original**"); // Keep this print statement
+	 *//***** DO NOT CHANGE THE CODE ABOVE THIS LINE *****/
+	/*
+	 * 
+	 * // WRITE YOUR CODE HERE FOR PART 1 for (int i = 0; i < arr.length; i++) {
+	 * System.out.println(arr[i]); arr[i]= arr[i] + x; }
+	 * 
+	 * 
+	 *//***** DO NOT CHANGE THE CODE BELOW THIS LINE *****/
+	/*
+	 * System.out.println("**New Array**"); // Keep this print statement
+	 *//***** DO NOT CHANGE THE CODE ABOVE THIS LINE *****//*
+															 * 
+															 * // WRITE YOUR CODE HERE FOR PART 2 for (int i = 0; i <
+															 * arr.length; i++) { System.out.println(arr[i]); }
+															 * 
+															 * }
+															 */
+
+	/*
+	 * static boolean whatsReturned(int num1, String words, boolean TF, double
+	 * price, char letter){ return; }
+	 * 
+	 */
+
+	/*
+	 * private static int rectanglePerimeter(int length, int width) {
+	 * 
+	 * // Formula for perimeter: 2 * (length + width) int perimeter=0; perimeter =
+	 * 2*(length + width);
+	 * 
+	 * return perimeter; }
+	 */
+
+	/*
+	 * private static int avgNameLength(String[] names) { int totalLength = 0; int
+	 * numNames = names.length;
+	 * 
+	 * // Loop through each name and accumulate its length for (String name : names)
+	 * { totalLength += name.length(); } numNames = totalLength / numNames ;
+	 * 
+	 * return numNames;
+	 * 
+	 * }
+	 */
+
+	/*
+	 * 
+	 * static String readyForTheBeach(int age){ if (age < 30){ return
+	 * "Only if you built Facebook or Google"; } if (age < 50){ return
+	 * "Perhaps if you lived a frugal life and worked a couple of jobs at a time.";
+	 * } if (age < 65){ return "You are getting closer..."; } else { return
+	 * "You can call it quits!"; } }
+	 */
+
+	/*
+	 * private static boolean passwordChecker(String password) {
+	 * 
+	 * boolean isValidPassword = false; char letter;
+	 * 
+	 * if (password.length()< 8) { isValidPassword=false; } else { for (int i = 0; i
+	 * < password.length() ;i++) { letter = password.charAt(i); if (letter == '!' ||
+	 * letter == '$' || letter == '%' || letter == '&') { isValidPassword=true;
+	 * break; }
+	 * 
+	 * }
+	 * 
+	 * }
+	 * 
+	 * 
+	 * return isValidPassword; }
+	 */
+
+	/*
+	 * static int sumOfDigits(int digits) { // WRITE YOUR CODE HERE int sum=0; while
+	 * (digits > 0) { int lastDigit = digits % 10; sum += lastDigit; digits /= 10; }
+	 * return sum;
+	 * 
+	 * }
+	 */
+
+	/*
+	 * private static int[] createArray(int a, int b, int c, int d, int e) { int[]
+	 * arrayName = new int[5];
+	 * 
+	 * arrayName[0]= a; arrayName[1]= b; arrayName[2]= c; arrayName[3]= d;
+	 * arrayName[4]= e;
+	 * 
+	 * return arrayName; }
+	 * 
+	 * 
+	 * }
+	 * 
+	 */
+
+	/*
+	 * private static double orderTotal(double product1, double product2, double
+	 * product3, double taxRate) {
+	 * 
+	 * double TotalPrice=0; double taxPortion=0;
+	 * 
+	 * TotalPrice = product1+product2+product3; taxPortion=TotalPrice*taxRate;
+	 * TotalPrice=TotalPrice+taxPortion;
+	 * 
+	 * return TotalPrice; }
+	 * 
+	 */
+
+	/*
+	 * static int returnSum(int x, int y, int z){ int answer = x + y + z; return x;
+	 * }
+	 */
+
+	/*
+	 * static void fizzBuzz(int num1) {
+	 * 
+	 * if (num1 % 3 == 0 && num1 % 5 == 0) { System.out.println("FizzBuzz"); } else
+	 * if (num1 % 3 == 0) { System.out.println("Fizz"); } else if (num1 % 5 == 0) {
+	 * System.out.println("Buzz"); } else { System.out.println("Hello world!"); }
+	 * 
+	 * }
+	 * 
+	 */
+
+	/*
+	 * private static int returnSum(int num1, int num2, int num3) {
+	 * 
+	 * int sum = 0; sum = num1+num2+num3; return sum; }
+	 */
+
+	/*
+	 * static String repeatWord(String userWord, int numTimes) {
+	 * 
+	 * String repeatWord = "";
+	 * 
+	 * for(int i=0;i< numTimes;i++) { repeatWord = repeatWord+userWord; }
+	 * 
+	 * return repeatWord;
+	 * 
+	 * 
+	 * }
+	 */
+
+	/*
+	 * static double divideNums(int a, int b){ return a/b; }
+	 */
 
 }
